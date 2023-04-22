f17c4e313f86b246d3fecf5bfac86f71071d42b1
Hamed Azaddoulat <82142255+DevelopmentBase@users.noreply.github.com>
2023-04-22 23:09:26 +0300

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,12 +5,5 @@ using namespace System;
 
 int main(array<String ^> ^args)
 {
-    int pre(0), cur(1), nxt(0);
-    do {
-        Console::WriteLine(pre);
-        nxt = pre + cur;
-        pre = cur;
-        cur = nxt;
-    } while (pre < 255);
     return 0;
 }
